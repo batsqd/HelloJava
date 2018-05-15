490c1a9b8b04d3654d4ea4c65b7e5a03ecf0f425
zhiyuan <2361660127@qq.com>
2018-05-15 11:55:37 +0800

    git  test

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,10 @@ public class Test {
     public static void main(String args[]){
         int sum=0;
         int a=1;
+        while(a<1){
+
+        }
+        int c=1;
         for(int i=1;i<=100;i++){
             sum+=i;
             f1();
